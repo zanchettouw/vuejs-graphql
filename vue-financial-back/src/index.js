85f5eca80f0beb32d7a0b2d6c4c6fa7020bf4a31
Fernando Zanchetta <fernandozanca94@gmail.com>
2020-08-14 07:37:34 -0300

    Create initial prisma/graphql config

---
+++ vue-financial-back/src/index.js
@@ -0,0 +1,32 @@
+const { GraphQLServer } = require('graphql-yoga')
+const Binding = require('prisma-binding')
+const { prisma } = require('./generated/prisma-client')
+
+const binding = new Binding.Prisma({
+  typeDefs: `${__dirname}/generated/generated/graphql-schema/schema.graphql`,
+  endpoint: process.env.PRISMA_ENDPOINT
+})
+
+const resolvers = {
+  Query: {
+    user(parent, args, context, info) {
+      // return prisma.user({ id: args.id })
+      //   .then(user => {
+      //     console.log('User: ', user)
+      //     return user
+      //   })
+      return binding.query.user({ where: { id: args.id } }, info)
+        .then(user => {
+          console.log('User: ', user)
+          return user
+        })
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: `${__dirname}/schema.graphql`,
+  resolvers
+})
+
+server.start().then(() => console.log('Server running on http://localhost:4000'))
